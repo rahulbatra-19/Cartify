7c90423ebd8d45be9cc26f5096a254b7a5f23fd5
rahulbatra-19 <batrarahul409@gmail.com>
2023-09-04 00:31:03 +0530

    some chanegs for deployment

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { Home, Page404, ProductPage, CartPage, PaymentGetaway } from "../pages";
 import { Provider } from "react-redux"; // Import Provider
 import AddProductForm from "../pages/AddProductForm";
 
 function App(props) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/");
+  }, []);
   return (
     <Provider store={props.store}>
       <div className="App">
